dbec97f9eca6fa0357208d12e5fb88739bb62731
bachmarek <marekbachmayer@gmail.com>
2020-09-17 06:45:08 +0000

    output script updated

--- server/output.py
+++ server/output.py
@@ -5,10 +5,15 @@ import ws_husa as husa
 import ws_marta as marta
 import ws_mdb as mdb
 import ws_ndb as ndb
+import ws_zahradbami as zahrad
 
 # ToDo
 # import ws_feste as feste
 # import ws_radost as radost
+# import ws_polar as polar
+# import ws_teren as teren
+# import ws_buran as buran
+# import ws_bolek as bolek
 
 output = []
 output.append(depog.depog_output)
@@ -21,7 +26,7 @@ output.append(artbar.artbar_output)
 # output.append(feste.feste_output)
 # output.append(radost.radost_output)
 # output.append(polar.polar_output)
-# output.append(zahrad.zahrad_output)
+output.append(zahrad.zahradbami_output)
 # output.append(teren.teren_output)
 # output.append(buran.buran_output)
 # output.append(bolek.bolek_output)
