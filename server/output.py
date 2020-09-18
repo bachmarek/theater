9023e5cb6765a3520abc3937d9ac1b050dbc9653
bachmarek <marekbachmayer@gmail.com>
2020-09-18 05:52:00 +0000

    webscraped Divadlo Polárka

--- server/output.py
+++ server/output.py
@@ -6,11 +6,9 @@ import ws_marta as marta
 import ws_mdb as mdb
 import ws_ndb as ndb
 import ws_zahradbami as zahrad
+import ws_polar as polar
 
 # ToDo
-# import ws_feste as feste
-# import ws_radost as radost
-# import ws_polar as polar
 # import ws_teren as teren
 # import ws_buran as buran
 # import ws_bolek as bolek
@@ -23,10 +21,15 @@ output.append(mdb.mdb_output)
 output.append(hadi.hadi_output)
 output.append(ndb.ndb_output)
 output.append(artbar.artbar_output)
-# output.append(feste.feste_output)
-# output.append(radost.radost_output)
-# output.append(polar.polar_output)
+output.append(polar.polar_output)
 output.append(zahrad.zahradbami_output)
 # output.append(teren.teren_output)
 # output.append(buran.buran_output)
 # output.append(bolek.bolek_output)
+
+## Out of order
+# import ws_feste as feste
+# import ws_radost as radost
+
+# output.append(feste.feste_output)
+# output.append(radost.radost_output)
---
+++ server/ws_polar.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup as soup
+import requests as req
+from datetime import datetime as dt
+
+my_url = 'https://divadlopolarka.cz/program/'
+
+html = req.get(my_url)
+
+page_html = html.text
+
+page_soup = soup(page_html, 'html.parser')
+containers = page_soup.findAll("div",{"class":"program__left-part"})
+dates = page_soup.findAll("h1",{"class":"underline"})
+
+theater = "polar"
+polar_output = []
+
+month_num = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_name = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+month_dict = dict(zip(month_name,month_num))
+
+## Rok
+year = page_soup.find("h1",{"class":"underline"}).text.strip().split()[1]
+
+for container in containers:
+## Měsíc
+  month_man = container.find("strong").text.split("/")[1]
+  month = str(month_dict[month_man])
+## Den
+  day = container.find("strong").text.split("/")[0]
+## Time
+  time = container.find("span").text.split()[2].replace(".", ":")+":00"
+## Název  
+  title_container = container.find("a")
+  if title_container.span:
+    title_container.span.extract()
+  title = title_container.text.strip()
+## Info
+  info = container.find("a")["href"]
+
+## Date string
+  date_string = time +" "+ day +" "+ month +" "+ year
+  date = dt.strptime(date_string, "%X %d %b %Y")
+
+## Output
+  keys = ['theater', 'date', 'title', 'info']
+  polar_row = dict(zip(keys, [theater, date, title, info]))
+  polar_output.append(polar_row)
