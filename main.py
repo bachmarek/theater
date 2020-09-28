42adca11832ca1dd5c55a94c104d447a1b6a478a
bachmarek <marekbachmayer@gmail.com>
2020-09-28 20:34:14 +0200

    flask server setup and test

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from common import output
+from flask import Flask
+from flask_restful import Api, Resource
 
-print(output.output_func())
+app = Flask(__name__)
+api = Api(app)
+
+names = {
+    "marek": {"age": 28, "gender": "male"},
+    "vendy": {"age": 26, "gender": "female"},
+}
+
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
+# dev only
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# print(output.output_func())
